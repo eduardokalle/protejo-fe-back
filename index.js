fcaa98b1b9716660d268885b94c1c25b6a087959
Eduardo Calle <49166547+eduardokalle@users.noreply.github.com>
2020-08-21 10:10:05 -0500

    remove port

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const express =  require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 4500;
-
 app.use(cors());
 
 const appRoutes = require('./routes');
 
 app.use('/', appRoutes);
 
-app.listen(port, () => {
-    console.log(`Servidor inicializado en el puerto 4500.  Accede desde la URL http://localhost:4500`);
-  });
+app.listen(process.env.PORT || 4500, () => {
+  console.log(`Servidor inicializado en el puerto 4500.  Accede desde la URL http://localhost:4500`);
+});
